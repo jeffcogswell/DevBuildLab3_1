b50295ddb58f01f2caed6c25093c9ee06da20b16
Jeff Cogswell <jeffcogs@gmail.com>
2021-07-26 15:41:32 -0400

    Initial commit

---
+++ Lab3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab3_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Lab 3.1: Student Information");
+			Console.WriteLine("Hello from lab 3.1");
+		}
+	}
+}
